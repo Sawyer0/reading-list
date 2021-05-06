16e3b727eba42ae29956a7958ca19e8d87218cf2
Dawan Sawyer <dawansawyer@gmail.com>
2021-05-06 10:49:54 -0400

    moved my files over into repo

---
+++ client/build/precache-manifest.23eb5401e37c3b8307da0df60cd50b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0b85fdee887aa692f93fd5ffbe40f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d801195d19bb1d8d2fd",
+    "url": "/static/css/main.1ef92e19.chunk.css"
+  },
+  {
+    "revision": "563812fdcfa95d36e049",
+    "url": "/static/js/2.dbbf1802.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.dbbf1802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d801195d19bb1d8d2fd",
+    "url": "/static/js/main.43f7ee52.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
